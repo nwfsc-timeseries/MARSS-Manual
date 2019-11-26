101f03c8c97499c7db90c3c7a43a34420784a7e8
EEH <eli.holmes@noaa.gov>
2019-11-26 11:30:10 -0800

    first build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# MARSS-Manual
- Manual for MARSS package.
+# Manual for the **MARSS** R package.
+ 
+ To see the 'Build Book' option on the build tab in RStudio, go Tools:Project Options:Build Tools and set the project type to 'Website'.  Make sure you have the **bookdown** package installed.
+
--- cleanDefsinRmd.R
+++ cleanDefsinRmd.R
@@ -19,19 +19,17 @@ replace.defs = function(defsfile, inputfile, outputfile){
 }
 
 basefiles=c(
-  "Lab-basic-matrix/basic-matrix-math",
-  "Lab-linear-regression/linear-regression-models-matrix",
-  "Lab-intro-to-ts/intro-to-ts",
-  "Lab-intro-to-ts/intro-ts-funcs-lab",
-  "Lab-dynamic-factor-analysis/intro-to-dfa",
-  "Lab-fitting-uni-ss-models/fitting-univariate-state-space",
-  "Lab-fitting-multi-ss-models/multivariate-ss",
-  "Lab-fitting-multi-ss-models/multivariate-ss-cov",
-  "Lab-fitting-multi-ss-models/multivariate-ss-missing-cov",
-  "Lab-fitting-DLMs/DLM",
-  "Lab-intro-to-jags/intro-to-jags",
-  "Lab-intro-to-stan/fitting-models-with-stan",
-  "Lab-box-jenkins-method/box-jenkins"
+  "src/01-introduction",
+  "src/02-quick-start",
+  "src/03-short-examples",
+  "src/04-input",
+  "src/05-output",
+  "src/06-residuals",
+  "src/07-conf-intervals",
+  "src/08-predictions",
+  "src/09-troubleshooting",
+  "src/10-em-algorithm",
+  "src/11-other-packages"
 )
 for(basefile in basefiles){
   filename=str_split(basefile,"/")[[1]][2]
@@ -39,38 +37,3 @@ for(basefile in basefiles){
   replace.defs("tex/defs.tex", paste(basefile,".Rmd",sep=""), outputfile)
 }
 
-#Create the R files
-require(stringr)
-for(basefile in basefiles){
-  filename=str_split(basefile,"/")[[1]][2]
-  outputfile = paste("docs/Rcode/",filename,".R",sep="")
-  knitr::purl(paste(basefile,".Rmd",sep=""), output= outputfile)
-}
-
-#Create the Rmd files
-require(stringr)
-for(basefile in basefiles){
-  filename=str_split(basefile,"/")[[1]][2]
-  inputfile = paste("cleanedRmd/",filename,".Rmd",sep="")
-  outputfile = paste("docs/Rmds/",filename,".Rmd",sep="")
-  file.copy(inputfile, outputfile, overwrite=TRUE)
-}
-
-####################################################
-# A search via google will show the following solutions
-# Add 'before_body' to your output.yml:
-#this works but the defs.tex is incl in html and flashes on screen
-# If the def file is long, the flash is very obvious.
-#
-# bookdown::gitbook:
-# before: |
-#   includes:
-#   before_body: [tex/bracket-start.txt, tex/defs.tex, tex/bracket-end.txt]
-# bookdown::pdf_book:
-#   includes:
-# before_body: tex/defs.tex
-# 
-# Another solution is to use the child arg in a chunk near the top
-# The problem is that it only works in math, so $ $ and $$ $$, not in equation, align or gather environments.
-# ```{r test-main, child = 'tex/defs.tex'}
-# ```
